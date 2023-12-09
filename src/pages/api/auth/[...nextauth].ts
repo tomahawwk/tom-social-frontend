475a34688c27688d8ed89bc198f41bec431089ba
tomahawwk <t.ahawwk@gmail.com>
2023-12-09 04:03:32 +0100

    adds chats

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -53,9 +53,9 @@ export default async function auth(req: NextApiRequest, res: NextApiResponse) {
 
             return {
               id: data.user.id.toString(),
-              email: data.user.email,
-              avatar: data.user.avatar?.url,
-              username: data.user.username,
+              email: data.user.attributes.email,
+              avatar: data.user.attributes.avatar?.url,
+              username: data.user.attributes.username,
               jwt: data.jwt,
             } as User;
           } catch (e) {
