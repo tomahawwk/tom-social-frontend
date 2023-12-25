8b40dac629e39be14b6a14cf0b6e362f471860ba
tomahawwk <t.ahawwk@gmail.com>
2023-12-26 00:09:37 +0100

    update

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,7 +1,6 @@
 import Auth from '@/components/Auth';
 import {Metadata} from 'next';
 import {NO_INDEX_PAGE} from '@/constants/seo.constants';
-import AuthLayout from '@/components/layout/AuthLayout';
 
 export const metadata: Metadata = {
   title: 'Register',
